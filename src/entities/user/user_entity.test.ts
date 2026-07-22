a2b9be01d8b9edcd9f0fb9890b760c465f264857
Rodrigo Takara <77634662+Rodrigo150500@users.noreply.github.com>
2026-07-22 09:58:54 -0300

    Updated user_entity.test.ts

--- src/entities/user/user_entity.test.ts
+++ src/entities/user/user_entity.test.ts
@@ -10,7 +10,7 @@ test("Should create an user entity", ()=>{
     const email = "Rodrigo.takara1505@gmail.com"
     const password_hash = "abc123"
     
-    const new_user = n ew UserEntity(first_name, last_name, email, password_hash)
+    const new_user = new UserEntity(first_name, last_name, email, password_hash)
     new_user.restore()
 
     expect(new_user.restore).toHaveBeenCalled()
